2c835572a7249af4c32870e8f93a0b959e7f1299
Noémie Laguelle <noemie.laguelle@gmail.com>
2020-12-29 13:44:08 +0100

    Few updates on form

--- my_model.py
+++ my_model.py
@@ -5,7 +5,6 @@ Created on Tue Dec 29 12:34:53 2020
 @author: caro9
 """
 
-from math import pi
 import pyomo.environ as pe
 
 def CirclePacking(size):
@@ -21,15 +20,14 @@ def CirclePacking(size):
     model.r = pe.Var(domain=pe.NonNegativeReals)
     
     # on a vu des méthodes permettant de déterminer le minimum
-    # mais on chercher max r ce qui revient à chercher min -r
-    
-    model.OBJ = pe.Objective(expr = - model.r ) 
+    # mais on cherche max r ce qui revient à chercher min -r
+    model.obj = pe.Objective(expr = - model.r ) 
     
     def constraint_rule(m,i,j):
         # return the expression for the constraint for i and j if i != j
         return ( (m.x[i] - m.x[j])^2 + (m.y[i] - m.y[j])^2 for j in m.J if j != i ) >= m.r^2
     
-    model.Constraint = pe.Constraint(model.I, rule = constraint_rule ) 
+    model.constraint = pe.Constraint(model.I, rule = constraint_rule ) 
     
     model.n = size
     
